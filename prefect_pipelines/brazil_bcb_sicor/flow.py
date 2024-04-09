e73e6e40bbdbb7e239777f0d587e54e2758a6e4b
folhesgabriel <pisagabriel09@gmail.com>
2024-04-09 19:32:16 -0300

    feat: add brazil_bcb_sicor_complemento_operacao_flow

--- prefect_pipelines/brazil_bcb_sicor/flow.py
+++ prefect_pipelines/brazil_bcb_sicor/flow.py
@@ -1,4 +1,4 @@
-from prefect import flow, Parameter
+from prefect import flow
 import os
 
 from prefect_pipelines.brazil_bcb_sicor.constants import Constants as bcb_contants
@@ -17,12 +17,13 @@ PATH_DATA = f'/home/gabri/brazil-rural-credit/tmp/output/microdados_2013.csv'
 
 #TODO: SET BUCKET, GCP, PROJECT_ID as env vars;
 
+
 @flow
-def brazil_bcb_sicor_microdados_operacoes_flow():
-    
-    DATASET_ID = Parameter("DATASET_ID", default="brazil_rural_credit")
-    TABLE_ID = Parameter("TABLE_ID", default="microdados_operacao")
-    DBT_MODEL_NAME = Parameter("DBT_MODEL_NAME", default="brazil_rural_credit__microdados_operacao.sql")
+def brazil_bcb_sicor_complemento_operacao_flow(
+    DATASET_ID: str = 'brazil_rural_credit', 
+    TABLE_ID: str = 'recurso_publico_complemento_operacao',
+    DBT_MODEL_NAME:str = "brazil_rural_credit__recurso_publico_complemento_operacao.sql"
+    ):
 
     
     download_files_task(
@@ -54,49 +55,12 @@ def brazil_bcb_sicor_microdados_operacoes_flow():
     run_dbt_model_task(model_name=DBT_MODEL_NAME)
 
 
-
 @flow
-def brazil_bcb_sicor_complemento_operacao_flow():
+def brazil_bcb_sicor_microdados_operacoes_flow(
+    DATASET_ID: str = 'brazil_rural_credit', 
+    TABLE_ID: str = 'microdados_operacao',
+    DBT_MODEL_NAME:str = "brazil_rural_credit__microdados_operacao.sql"):
     
-    DATASET_ID = Parameter("DATASET_ID", default="brazil_rural_credit")
-    TABLE_ID = Parameter("TABLE_ID", default="recurso_publico_complemento_operacao")
-    DBT_MODEL_NAME = Parameter("DBT_MODEL_NAME", default="brazil_rural_credit__recurso_publico_complemento_operacao.sql")
-    
-    download_files_task(
-        urls=bcb_contants.URLS.value[TABLE_ID],
-        folder=bcb_contants.INPUT_FOLDER.value,
-        max_parallel=5,
-    )
-    
-    pre_process_files_task(
-        folder=bcb_contants.INPUT_FOLDER.value,
-        output_folder=bcb_contants.OUTPUT_FOLDER.value,
-        table_id=TABLE_ID,
-    )
-    
-    upload_to_gcs_task(
-        bucket_name=BUCKET,
-        dataset_id=DATASET_ID,
-        table_id=TABLE_ID,
-        path=bcb_contants.OUTPUT_FOLDER.value,
-    )
-    
-    create_dataset_and_table_with_inferred_schema_task(
-        project_id=PROJECT_ID,
-        dataset_id=DATASET_ID,
-        table_id=TABLE_ID,
-        bucket_name=BUCKET,
-    )
-    
-    run_dbt_model_task(model_name=DBT_MODEL_NAME)
-
-
-@flow
-def brazil_bcb_sicor_microdados_operacoes_flow():
-    
-    DATASET_ID = Parameter("DATASET_ID", default="brazil_rural_credit")
-    TABLE_ID = Parameter("TABLE_ID", default="microdados_operacao")
-    DBT_MODEL_NAME = Parameter("DBT_MODEL_NAME", default="brazil_rural_credit__microdados_operacao.sql")
     
     download_files_task(
         urls=bcb_contants.URLS.value['microdados_operacao'],
@@ -128,4 +92,4 @@ def brazil_bcb_sicor_microdados_operacoes_flow():
 
 
 
-brazil_bcb_sicor_microdados_operacoes_flow()
+brazil_bcb_sicor_complemento_operacao_flow()
